ff06e12b431559f07081e349f4635069c0d3a183
5TpNowickaMonika <egzamin@poczta.pl>
2025-09-30 08:41:49 +0200

    30.09.2025

--- main.py
+++ main.py
@@ -10,9 +10,31 @@ class MyForm(QDialog):
         super().__init__()
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
-
+        self.ui.rgbR.valueChanged.connect(self.slider_change)
+        self.ui.rgbG.valueChanged.connect(self.slider_change)
+        self.ui.rgbB.valueChanged.connect(self.slider_change)
+        self.ui.take.clicked.connect(self.little_rectangle)
         self.show()
 
+    def slider_change(self):
+        r = self.ui.rgbR.value()
+        g = self.ui.rgbG.value()
+        b = self.ui.rgbB.value()
+
+        self.ui.rgbRNum.setText(str(r))
+        self.ui.rgbGNum.setText(str(g))
+        self.ui.rgbBNum.setText(str(b))
+
+        self.ui.rgbRectangle.setStyleSheet("background-color: rgb({}, {}, {});".format(r, g, b))
+
+    def little_rectangle(self):
+        r = self.ui.rgbR.value()
+        g = self.ui.rgbG.value()
+        b = self.ui.rgbB.value()
+
+        if self.ui.take.clicked:
+            self.ui.rgbTake.setStyleSheet("background-color: rgb({}, {}, {});".format(r, g, b))
+            self.ui.rgbTake.setText(f"{r}, {g}, {b}")
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = MyForm()
